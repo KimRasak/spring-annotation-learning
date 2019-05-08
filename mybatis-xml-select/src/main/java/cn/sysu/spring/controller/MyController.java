bfbd126ee6bf55bcbcb3a7e5d4ec05637f77a5af
Rasak <657278923@qq.com>
2019-05-08 13:19:46 +0800

    删除无用行

--- mybatis-xml-select/src/main/java/cn/sysu/spring/controller/MyController.java
+++ mybatis-xml-select/src/main/java/cn/sysu/spring/controller/MyController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
