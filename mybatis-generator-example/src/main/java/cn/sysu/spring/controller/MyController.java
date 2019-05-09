4ee8b631bc0454fb41022e55a5038038b5c82023
KimRasak <657278923@qq.com>
2019-05-09 08:41:32 +0800

    mybatis generator example

---
+++ mybatis-generator-example/src/main/java/cn/sysu/spring/controller/MyController.java
@@ -0,0 +1,19 @@
+package cn.sysu.spring.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class MyController {
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping
+    public List<User> queryAllUser() { return userMapper.queryAllUser(); }
+}
